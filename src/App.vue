38e4e211520fa47fa6189ae07c19dd6c079a1b62
David Gao <david.gjw@outlook.com>
2019-04-11 08:46:09 +0800

    Remove test code

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,12 @@
 </template>
 
 <script>
-  import { Base64 } from 'js-base64'
-  import axios from 'axios'
 
-  const pako = require('pako');
   export default {
     name: 'App',
     created() {
       this.$store.dispatch('GetCosUrl').then(() => {
       }).catch(err => {
-      })
-      axios.get('http://192.168.1.44:8080/aggregation/getCompress').then(response => {
-        const data = response.data.data.result
-        console.log(data.name)
-        const encodedContent = Base64.decode(data.content)
-        const content = pako.ungzip(encodedContent)
-        console.log('' + content)
       })
     }
   }
