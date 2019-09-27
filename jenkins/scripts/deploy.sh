fadd2f3dc2f6517422c02fc65085af46d0485514
David Gao <david.gao@weesharing.com>
2019-09-27 18:10:40 +0800

    Update jenkins for Wuxi

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 set user smartadmin
 set passwd Smartautotech@123
-set host 192.168.200.37
-set port 22
+set host 119.3.3.226
+set port 22140
 set src_dir ./
 set tag_dir /data/server/wuxiadmall
 set name dist.zip
@@ -11,6 +11,7 @@ set tmp_dir /tmp
 
 ##拷贝dist.zip文件到目标机器
 spawn sh -c "zip -r dist.zip dist"
+expect "$ "
 spawn sh -c " scp -P $port -r $src_dir$name $user@$host:$tag_dir"
 expect "password:"
 send "${passwd}\n"
